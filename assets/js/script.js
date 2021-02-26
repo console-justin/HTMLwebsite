7fe391931d2ace9110fc76088c48ef7a5258c3fe
console-justin <58694791+console-justin@users.noreply.github.com>
2021-02-26 13:45:58 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-<script src="https://cdn.jsdelivr.net/npm/algoliasearch@4.5.1/dist/algoliasearch-lite.umd.js"></script>
+const searchClient = algoliasearch('VDOL4AYORQ', 'de9251d81fdfbf7b41201a6aa8fcc238');
+
+const search = instantsearch({
+indexName: 'Email-Index',
+searchClient,
+});
+
+search.addWidgets([
+instantsearch.widgets.searchBox({
+container: '#searchbox',
+}),
+
+instantsearch.widgets.configure({
+hitsPerPage: 16
+}),
+
+instantsearch.widgets.hits({
+container: '#hits',
+templates: {
+item: `
+<div>
+<div class="hit-name">
+{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+</div>
+</div>
+`,
+},
+}),
+
+instantsearch.widgets.pagination({
+container: '#pagination',
+}),
+]);
+
+search.start();
